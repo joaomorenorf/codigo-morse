a3606d20451242f05e25724377fc499ffaa97ee5
João Moreno <joaomorenorf@ufmg.br>
2017-08-18 17:38:53 -0300

    Limpei o código

--- Morse.ino
+++ Morse.ino
@@ -1,57 +1,50 @@
 
+// Pino do LED
 int LED = 7;
 
+// Mensagem que será transmitida
 char codigo[] = "sos";
+
+// Variáveis para calibração
+int tempo_curto = 200;
+int tempo_longo = 600;
+int tempo_e_toques = 200;
+
 int contador = 0;
 
-void setup ( ) {
-  pinMode(LED, OUTPUT);
+void setup () {
+    pinMode(LED, OUTPUT);
 }
 
-void loop ( ) {
-  if (codigo[contador] == 's') { //S
+void curto () {
     digitalWrite(LED, HIGH);
-    delay(200);
+    delay(tempo_t_curto);
     digitalWrite(LED, LOW);
-    delay(200);
-  
-    digitalWrite(LED, HIGH);
-    delay(200);
-    digitalWrite(LED, LOW);
-    delay(200);
-  
-    digitalWrite(LED, HIGH);
-    delay(200);
-    digitalWrite(LED, LOW);
-    delay(200);
-  
-    delay(200);
-  }
-  
-  if (codigo[contador] == 'o') { //O
-    digitalWrite(LED, HIGH);
-    delay(600);
-    digitalWrite(LED, LOW);
-    delay(200);
-  
-    digitalWrite(LED, HIGH);
-    delay(600);
-    digitalWrite(LED, LOW);
-    delay(200);
-  
-    digitalWrite(LED, HIGH);
-    delay(600);
-    digitalWrite(LED, LOW);
-    delay(200);
-    delay(200);
+    delay(tempo_e_toques);
+}
 
-  }
-  
-  contador++;
-  
-  if (codigo[contador] == '\0') { //FIM
-    contador = 0;
-    delay(2000);
-    
-  }
+void longo () {
+    digitalWrite(LED, HIGH);
+    delay(tempo_t_longo);
+    digitalWrite(LED, LOW);
+    delay(tempo_e_toques);
+}
+
+void loop () {
+    if (codigo[contador] == 's') { //S
+        curto(); curto(); curto();
+        delay(200);
+    }
+
+    if (codigo[contador] == 'o') { //O
+        longo(); longo(); longo();
+        delay(200);
+    }
+
+    contador++;
+
+    if (codigo[contador] == '\0') { //FIM
+        contador = 0;
+        delay(2000);
+    }
 }
